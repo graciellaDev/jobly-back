e154c0b5510d3b07cfb9abed57a36078b63f719e
Alesja Bezrukova <gravielladesign@gmail.com>
2025-02-16 11:13:30 +0300

    comment jwt register

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -65,7 +65,6 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        var_dump('dfgdfgdfgdfgdf');
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
