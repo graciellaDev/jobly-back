0e00a3ceca786205cb99f346cb71bd20cdee78bc
Alesja Bezrukova <gravielladesign@gmail.com>
2025-06-22 17:42:30 +0300

    add api hh.ru auth and profile

--- routes/api-hh.php
+++ routes/api-hh.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\api\HeadHunterController ;
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/auth', [HeadHunterController::class, 'auth']);
     Route::get('/code', [HeadHunterController::class, 'code']);
+    Route::get('/profile', [HeadHunterController::class, 'getProfile']);
 });
 
