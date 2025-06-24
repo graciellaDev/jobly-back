b350b94cd8ff98b84ab9b2311b43a84b08b88cae
Alesja Bezrukova <gravielladesign@gmail.com>
2025-06-24 23:21:26 +0300

    edit api hh.ru

--- config/hh.php
+++ config/hh.php
@@ -9,8 +9,7 @@
   |
 */
 return [
-    'client_id' => '
-IJL4TE2E48Q3CGCHQUGR7005VSAJ52SI8SJET21COIDCFCK88LJDIFFIJD3TJ23F',
+    'client_id' => 'IJL4TE2E48Q3CGCHQUGR7005VSAJ52SI8SJET21COIDCFCK88LJDIFFIJD3TJ23F',
     'client_secret' => 'LJ2S9F3JBODQNC2S35EFK48O9ITL8N6KE2M9HTAUEOCIBV0FPI927EH0DCBRQGCQ',
     'auth_url' => 'https://hh.ru/oauth/authorize',
     'redirect_url' => env('APP_URL') . '/api/code-hh',
